ac7cc6a66bb40d6d271711a641fceadb2e181c1f
George Eracleous <basketballcy@gmail.com>
2012-02-05 10:36:09 +0000

    Added method for extracting the top keywords from the index

--- analysis/index.py
+++ analysis/index.py
@@ -9,8 +9,8 @@ from lucene import *
 class Index(object):
     '''
     This class is responsible for using the Pylucene framework
-    for building an index for the keywords appearing in the documents
-    we want to analyse.
+    to build an index for the keywords appearing in the documents
+    we want to analyse. It also provides methods to search the index
     '''
     
     def __init__(self, index_dir):
@@ -39,7 +39,7 @@ class Index(object):
             doc.add(lucene.Field("id", str(document.id),
                                 lucene.Field.Store.YES,
                                 lucene.Field.Index.NOT_ANALYZED))
-            doc.add(lucene.Field("content", document.content['raw'],
+            doc.add(lucene.Field("content", ' '.join(document.content['tokens']),
                                 lucene.Field.Store.YES,
                                 lucene.Field.Index.ANALYZED))
             doc.add(lucene.Field("author", document.author_screen_name,
@@ -59,23 +59,52 @@ class Index(object):
         self.writer.optimize()
         self.writer.close()
         
-    def search(self, query):
+    def search(self, query, limit=50):
         '''
         Searches the index based on the query supplied.
         '''
         directory = lucene.SimpleFSDirectory(lucene.File(self.index_dir))
-        searcher = lucene.IndexSearcher(directory, True)    
-        query = lucene.QueryParser(lucene.Version.LUCENE_CURRENT, "content",
-                                self.analyser).parse(query)
-        scoreDocs = searcher.search(query, 50).scoreDocs
+        searcher = lucene.IndexSearcher(directory, True)  
         
-        results = []
-        for scoreDoc in scoreDocs:
-                results.append(searcher.doc(scoreDoc.doc))
+        try:  
+            query = lucene.QueryParser(lucene.Version.LUCENE_CURRENT, "content",
+                                    self.analyser).parse(query)
+            scoreDocs = searcher.search(query, limit).scoreDocs
+            results = []
+            for scoreDoc in scoreDocs:
+                    results.append(searcher.doc(scoreDoc.doc))
+        except lucene.JavaError, e:
+            print e
                 
         searcher.close()
         return results
+    
+    def get_top_keywords(self, limit=10):
+        '''
+        Returns the top keywords, in terms of documents mentioning them
+        in the index. 
+        '''
+        #=======================================================================
+        # directory = lucene.SimpleFSDirectory(lucene.File(self.index_dir))
+        # searcher = lucene.IndexSearcher(directory, True)
+        # 
+        # #Please note that we had to default number of hits to a large number
+        # #because search needs the number of the hits to return as an argument.
+        # #Since we want all the results  
+        # hits = searcher.search(lucene.MatchAllDocsQuery(), 10**6)
+        # return hits.scoreDocs
+        #=======================================================================
+        directory = lucene.SimpleFSDirectory(lucene.File(self.index_dir))
+        reader = lucene.FilterIndexReader.open(directory, True)
+        terms = reader.terms()
         
+        term_freqs = []
+        while terms.next():
+            term = terms.term();
+            if term.field() == 'content':
+                term_freqs.append( (reader.docFreq(term), term.text()) )
+        return sorted(term_freqs, reverse=True)[:limit]
+    
 class PorterStemmerAnalyzer(lucene.PythonAnalyzer):
     '''
     This class extends the simple analyzer by adding a stemmer. 
--- tests/index_tests.py
+++ tests/index_tests.py
@@ -14,13 +14,14 @@ from database.model.tweets import TwoGroupsTweet
 ws = WarehouseServer()
 sample_docs = ws.get_n_documents(100, type=TwoGroupsTweet)
 
+index = Index("index")
+for doc in sample_docs:
+    index.add_document(doc)
+index.finalize()
+
 class TestPlayground(unittest.TestCase):
   
-    def test_indexing(self):
-        index = Index("index")
-        for doc in sample_docs:
-            index.add_document(doc)
-        index.finalize()
+    def test_searching(self):        
         results = index.search("sales")
         
         calculated = []
@@ -30,6 +31,13 @@ class TestPlayground(unittest.TestCase):
         expected = ['4f2d602780286c38a7000013', '4f2d603280286c38a700001e']
 
         self.assertEqual(expected, calculated)
+    
+    def test_top_terms_index(self):
+        results = index.get_top_keywords(10)
+        expected = [(52, u'uk'), (8, u'us'), (8, u'new'), (6, u'week'), (5, u'last'), (5, u'host'), (4, u'yeah'), (4, u'want'), (4, u'presid'), (4, u'nation')]
+        self.assertEquals(expected, results)
+        
+    
 
 if __name__ == "__main__":
     unittest.main()
