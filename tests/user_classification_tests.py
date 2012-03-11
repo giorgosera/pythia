dc39bc73b80d782f91740c5718f5a164b3ed855a
George Eracleous <basketballcy@gmail.com>
2012-03-11 15:39:28 +0000

    Added extended classification tests

--- tests/user_classification_tests.py
+++ tests/user_classification_tests.py
@@ -33,5 +33,47 @@ class Test(unittest.TestCase):
         prediction = treeClassifier(example)
         self.assertEquals(0, prediction.value)
         
+    def test_author_classification_real_dataset(self):
+        # 0 --> Celebrirty
+        # 1 --> Media
+        # 2 --> Journalists
+        # 3 --> Common people
+        
+        train_set = numpy.array([[0.18, 0.57, 0.01,  0.053, 0.0,  52872.,  0], #Bill Gates
+                                [0.5, 0.1, 0.0, 0.09, 0.0,  151.,  0], #Justin Bieber
+                                [0.096, 0.4, 0.06, 0.2, 0.0, 14052.,  0], #Ashton Kutcher
+                                [0.06, 0.051, 0.04,  0.62, 0.0,  216342.,   0], #Oprah
+                                [0.026, 0.72, 0.03,  0.07, 0.0,  79183.,  0], #Amy Winehouse
+                                [0.006, 0.85, 0.52,  0.0, 0.0,  55025., 1], #BBC
+                                [0.17, 0.77, 0.86,  0.03, 0.0,  5540., 1], #CNN
+                                [0.25, 0.73, 2.,  0.0, 0.0,  264., 1], #HuffPost
+                                [0.02, 0.99, 0.79,  0.03, 0.0,  5034., 1], #AlJazeera
+                                [0.13, 0.31, 2.2,  0.33, 0.0,  14., 2], #Ali Velshi
+                                [0.19, 0.19, 2.8,  0.17, 0.0,  4., 2], #MujMash 
+                                [0.053, 0.23, 2.5,  0.3, 0.0,  12., 2], #StevePoliti
+                                [0.09, 0.16, 11.3,  0.36, 0.0,  8., 2], #Rachel King
+                                [0.4, 0.36, 8.6,  0.16, 0.0,  0.32, 3], #George Eracleous
+                                [0.2, 0.12, 7.1,  0.35, 0.0,  0.38, 3], #Nik Adhia
+                                [0.26, 0.15, 2.4,  0.31, 0.0,  0.9, 3], #A person
+                                [0.55, 0.13, 13.2,  0.25, 0.0,  0.7, 3]]) #Yet another person
+        
+        attributes = ["retweets", "links", "retweeted", "replies", "mentions", "ff-ratio", "class"]
+    
+        table = construct_orange_table(attributes, train_set, classed=True)
+        treeLearner = orngTree.TreeLearner()        
+        treeClassifier = treeLearner(table)    
+        celebrity_example = Orange.data.Instance(table.domain, [0.096, 0.3, 0.03,  0.08, 0.0,  94258,  0]) 
+        media_example = Orange.data.Instance(table.domain, [0.0, 0.8, 0.89,  0.031, 0.0,  184.,  0])    
+        common_example = Orange.data.Instance(table.domain, [0.55, 0.13, 13.2,  0.25, 0.0,  0.7,  0])    
+        journalist_example = Orange.data.Instance(table.domain, [0.24, 0.48, 14.7,  0.11, 0.0,  11.9,  0])    
+        prediction_celebrity = treeClassifier(celebrity_example)
+        prediction_media = treeClassifier(media_example)
+        prediction_journalist = treeClassifier(journalist_example)
+        prediction_common = treeClassifier(common_example)
+        calculated = [prediction_celebrity, prediction_media, prediction_journalist, prediction_common]
+        expected = [0, 1, 2, 3]
+        self.assertEqual(expected, calculated)
+        #orngTree.printTree(treeClassifier)
+        
 if __name__ == "__main__":
     unittest.main()
